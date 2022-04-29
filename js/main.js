700a6220fa64f1fa1acd530cbc8bbe27f9fef8ef
devbyjonah <jonahwagner231@gmail.com>
2022-04-29 16:45:30 -0700

    added documentation to main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* init function initializes the default drinks on the page by looping a request to the API for a random drink */
+
 let init = function() {
 
 	for (let i = 0; i < 40; i++){
@@ -5,6 +7,7 @@ let init = function() {
 			.then(res => res.json())
 			.then(data => {
 
+					/* create drink cards */
 					let newLink = document.createElement('a')
 					let newImg = document.createElement('img')
 
@@ -14,6 +17,7 @@ let init = function() {
 					newImg.src = url
 					newLink.href = url
 
+					/* insert card into DOM under main element */
 					document.querySelector('main').appendChild(newLink)
 			})
 			.catch(error => {
@@ -22,12 +26,17 @@ let init = function() {
 	}
 }
 
+/* fetch drinks is called on user searching */
+
 let fetchDrinks = function(){	
+
+	/* assign drink variable to user input */
 	let drink = document.querySelector('input').value
 
+	/* clear main element of all children */
 	document.querySelector('main').replaceChildren()
 
-
+	/* search for inputted drink */
 	fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
 		.then(res => res.json())
 		.then(data => {
@@ -35,6 +44,7 @@ let fetchDrinks = function(){
 
 			for (let i = 0; i < data.drinks.length; i++){
 
+				/* create drink cards */
 				let newLink = document.createElement('a')
 				let newImg = document.createElement('img')
 
@@ -44,6 +54,7 @@ let fetchDrinks = function(){
 				newImg.src = url
 				newLink.href = url
 
+				/* insert card to DOM under main element */
 				document.querySelector('main').appendChild(newLink)
 			}
 		})
@@ -52,5 +63,6 @@ let fetchDrinks = function(){
 		})
 }
 
+/* calls init after DOM has fully loaded (doesn't wait for stylesheets) */
 document.addEventListener('DOMContentLoaded', init)
 document.querySelector('button').addEventListener('click', fetchDrinks)
